9e284629d78ce029e3e5c37bfc2975cdcb4e9741
Walid Budgaga <wbudgaga@gmail.com>
2020-07-11 14:37:08 -0600

    Update ThreadPoolManager.java

--- src/cs555/nc/threadpool/ThreadPoolManager.java
+++ src/cs555/nc/threadpool/ThreadPoolManager.java
@@ -7,14 +7,14 @@ import cs555.nc.tasks.Task;
 
 // Starts and manages the work between workers that execute taks
 public class ThreadPoolManager{
-	private Worker[] 		workers;
-	private Queue<Task> 	taskQueue 		= new LinkedList<Task>();
+	private Worker[] 	workers;
+	private Queue<Task> 	taskQueue 	= new LinkedList<Task>();
 	private Queue<Worker> 	workerQueue 	= new LinkedList<Worker>();
 	
 	public ThreadPoolManager(int numberOfThreads) {
-		workers 		= new Worker[numberOfThreads];
+		workers 			= new Worker[numberOfThreads];
 		for (int i = 0; i < numberOfThreads; ++i) {
-			workers[i] 	= new Worker(this, i);
+			workers[i] 		= new Worker(this, i);
 		}
 	}
 		
@@ -39,8 +39,8 @@ public class ThreadPoolManager{
 	
 	private  void assignTasks(){
 		if (taskQueue.size() > 0 && workerQueue.size()>0){
-			Task task = taskQueue.poll();
-			Worker workerThread = workerQueue.poll();
+			Task task 		= taskQueue.poll();
+			Worker workerThread 	= workerQueue.poll();
 			workerThread.assignTask(task);
 		}
 	}
