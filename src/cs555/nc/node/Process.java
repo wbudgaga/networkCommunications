29a12d557433aaaed3cf6773772d3d8e54d8eb72
Walid Budgaga <wbudgaga@gmail.com>
2020-07-11 14:28:34 -0600

    Update Process.java

--- src/cs555/nc/node/Process.java
+++ src/cs555/nc/node/Process.java
@@ -14,23 +14,23 @@ public class Process {
 	private int 		port;		 	// port the peer is listening on
 	
 	public Process(int port){
-		dataTracker 		= new DataTracker();
-		this.port 		= port;
+		dataTracker 			= new DataTracker();
+		this.port 			= port;
 	}
 	
 	private ReceivingHandler startReceiving() throws IOException{
-		ReceivingHandler rHandler = new ReceivingHandler(dataTracker,port);
+		ReceivingHandler rHandler 	= new ReceivingHandler(dataTracker,port);
 		rHandler.start();
 		return rHandler;
 	}
 	
 	private void startSending() throws IOException{
-		SendingHandler sHandler = new SendingHandler(dataTracker);
+		SendingHandler sHandler 	= new SendingHandler(dataTracker);
 		sHandler.start();
 	}
 
 	private void start(boolean startSending) throws IOException, InterruptedException{
-		ReceivingHandler rHandler = startReceiving();
+		ReceivingHandler rHandler 	= startReceiving();
 		while(!startSending && !dataTracker.hasReceived());
 		startSending();
 		Thread.sleep(Setting.WAIT_TIME_TO_STOP);
@@ -46,7 +46,7 @@ public class Process {
 		}
 		
 		int port = Integer.parseInt(args[0]);
-		boolean startSending = false;/* Indicator that determines if this peer should start sending */
+		boolean startSending 		= false;	/* Indicator that determines if this peer should start sending */
 		if (args.length == 2 && args[1].compareToIgnoreCase("sending")==0)
 			startSending = true;
 			
